4f4d5a1a3a8ee96d4ca8c6cd960e519408102711
Barış Suna <barsuna02@hotmail.com>
2023-10-05 22:00:30 +0300

    Added firebase and authentication

--- app/src/main/java/com/barissuna/chatapp/LoginFragment.kt
+++ app/src/main/java/com/barissuna/chatapp/LoginFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.barissuna.chatapp.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +35,23 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.signUpButton.setOnClickListener{
+            auth.createUserWithEmailAndPassword(binding.emailText.text.toString(),binding.passwordText.text.toString()).addOnSuccessListener {
+                val action = LoginFragmentDirections.actionLoginFragmentToChatFragment()
+                findNavController().navigate(action)
+            }.addOnFailureListener {
+                Toast.makeText(requireContext(),it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }
+
+        binding.logInButton.setOnClickListener{
+            auth.signInWithEmailAndPassword(binding.emailText.text.toString(),binding.passwordText.text.toString()).addOnSuccessListener {
+                val action = LoginFragmentDirections.actionLoginFragmentToChatFragment()
+                findNavController().navigate(action)
+            }.addOnFailureListener {
+                Toast.makeText(requireContext(),it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }
 
     }
 
